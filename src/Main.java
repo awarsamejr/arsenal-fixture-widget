cfc103b863fed68d2be5a4d0c0443ee35176bbe9
awarsamejr <warsamejr21@gmail.com>
2024-12-07 11:48:52 +0100

    Made a method for file reading and added a search function to seach by opponent

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static ArrayList<Fixture> schedule = new ArrayList<>();
     public static void main(String[] args) {
         System.out.println("Welcome to the Arsenal Fixture Widget!");
-        ArrayList<Fixture> schedule = new ArrayList<>();
         
-        String filePath = "arsenal-fixture-widget/Fixtures.csv";
+
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Search for oppnonent fixture: ");
+        
+        String opponent = scanner.nextLine();
+        fileReader("arsenal-fixture-widget/Fixtures.csv");
+        searchOpponent(schedule, opponent);
+        
                 
+    }
+    public static void fileReader(String filePath){
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
 
             String line;
@@ -22,14 +32,27 @@ public class Main {
                 schedule.add(fixture);
             }
 
-            for (Fixture x : schedule){
-                System.out.println(x);
-            }
-
         }catch (FileNotFoundException e) {
             System.out.println("File not found: " + filePath);
         } catch (Exception e){
             System.out.println("An error occured: " + e.getMessage());
+        }
+    }
+    public static void searchOpponent(ArrayList<Fixture> schedule, String opponent){
+        if (schedule.isEmpty()){
+            System.out.println("No fixtures available");
+            return;
+        }
+        boolean found = false;
+        for (Fixture games : schedule){
+            if (games.team.equalsIgnoreCase(opponent)){
+                System.out.println(games);
+                found = true;
+            }
+        }
+        if (!found){
+            
+                System.out.println("No Fixtures found for given opponent: " + opponent);
         }
     }
 }
