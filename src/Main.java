d07f93118b2a7401058875cfd2bb69a874c1dbe9
awarsamejr <warsamejr21@gmail.com>
2024-12-07 11:12:33 +0100

    Added a file reader to read fixtures

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,35 @@
 import java.time.LocalDate;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Arsenal Fixture Widget!");
         ArrayList<Fixture> schedule = new ArrayList<>();
-        LocalDate fixtureDate = LocalDate.of(2024,12,9);
+        
+        String filePath = "arsenal-fixture-widget/Fixtures.csv";
+                
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
 
-        Fixture LivArs = new Fixture(fixtureDate,"Liverpool","Home");
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] biter = line.split(",");
+                LocalDate date = LocalDate.parse(biter[0]);
+                String team = biter[1];
+                String location = biter[2];
 
-        System.out.println(LivArs);
+                Fixture fixture = new Fixture(date, team, location);
+                schedule.add(fixture);
+            }
+
+            for (Fixture x : schedule){
+                System.out.println(x);
+            }
+
+        }catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filePath);
+        } catch (Exception e){
+            System.out.println("An error occured: " + e.getMessage());
+        }
     }
 }
